f374cd4ff04d8bb4a3c21bce466818bae463990e
Najiluc <33139457+NAJILUC@users.noreply.github.com>
2021-06-28 09:04:22 -0500

    R4

---
+++ src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+
+import com.example.demo.MyUserDetails;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.models.User;
+ 
+public class UserDetailsServiceImpl implements UserDetailsService {
+ 
+    @Autowired
+    private UserRepository userRepository;
+     
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+         
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+         
+        return new MyUserDetails(user);
+    }
+ 
+}
